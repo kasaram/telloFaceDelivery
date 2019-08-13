38d7e1a1cbfec05fb706ee8ce742fcfe10dd73e5
lucafluri <luca.fluri4@gmail.com>
2019-08-13 12:17:11 +0200

    important fix

--- TelloTV.py
+++ TelloTV.py
@@ -287,7 +287,7 @@ class FrontEnd(object):
                 for face_encoding in face_encodings:
                     # See if the face is a match for the known face(s)
                     matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
+                    name = "Who dis?"
         
                     # Use the known face with the smallest distance to the new face
                     face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
