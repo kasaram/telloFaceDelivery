de15f568332e2967806551ffe09bfb9501191b3d
Dario Breitenstein <online@imakethings.ch>
2019-08-15 13:12:38 +0200

    remove temp.py

--- telloFaceDelivery.py
+++ telloFaceDelivery.py
@@ -450,6 +450,7 @@ def video_feed():
    """Video streaming route. Put this in the src attribute of an img tag.""" 
    return Response(video_gen(), mimetype='multipart/x-mixed-replace; boundary=frame') 
 
+
 if __name__ == '__main__': 
     drone = DroneControl()
     app.run(host='0.0.0.0', debug=False, threaded=True)
